391ee9fe25bdebc46472ea55e69cdd412ba9d6d3
cutelixin <792075865@qq.com>
2024-03-27 23:23:11 +0800

    首次提交

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="box">
+    <h1>我是app</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+</script>
+
+<style scoped>
+.box {
+  width: 600px;
+  height: 400px;
+  background-color: black;
+
+  h1 {
+    color: white;
+  }
+}
+</style>
